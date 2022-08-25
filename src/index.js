a8b98688ce4fa62acce3d3e38b95e3fcc288d2e3
anakarina1928 <anakarina.nak@gmail.com>
2022-08-25 00:46:05 -0500

    funcion para el calculo de los gastos de las vacaciones

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const vacationCalc = document.getElementById('vacationCalc');
 
 
 const calculateExpenses = () => {
+    // debo refactorizar, colocar los id dentro de una funcion y luego desestructurarla en un return
     const destiny = document.getElementById('destiny').value;
     const budget = document.getElementById('budget').value;
     const acomodation = document.getElementById('acomodation').value;
@@ -9,13 +10,19 @@ const calculateExpenses = () => {
     const food = document.getElementById('food').value;
 
     // utilizamos parsenint()para asegurarnos que todo lo que sea ingresado sea de valor numerico
-let expenses = parseInt(acomodation ) + parseInt(transport) + parseInt(food);
+    let expenses = parseInt(acomodation) + parseInt(transport) + parseInt(food);
 
-let balance= budget - expenses;
-console.table('presupueto: ', budget, 'resta de todos los input con  presupuesto: ',balance,'destino: ', destiny);
+    let balance = budget - expenses;
+    console.table('presupueto: ', budget, 'resta de todos los input con  presupuesto: ', balance, 'destino: ', destiny);
+}
+
+
+
+const resultsPrintout = () => {
+    const result = document.getElementById('result');
+    //debo crear un template donde ira el relsultado. esta funcion debe ser llamada en calculateExpenses
+
 }
 
 //El evento submit permite iniciar el envío del formulario
 vacationCalc.addEventListener('submit', calculateExpenses);
-
-const 
